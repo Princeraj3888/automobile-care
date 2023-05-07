4208049dc64f2778a533581aec1917037476c5cd
Princeraj3888 <luckyraj2307@gmail.com>
2023-05-07 20:08:38 +0100

    initial stage of automobile care api gateway

---
+++ automobile-api-gateway/src/main/java/com/automobilecare/automobileapigateway/AutomobileApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.automobilecare.automobileapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobileApiGatewayApplication.class, args);
+	}
+
+}
---
+++ automobile-api-gateway/src/test/java/com/automobilecare/automobileapigateway/AutomobileApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automobilecare.automobileapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
